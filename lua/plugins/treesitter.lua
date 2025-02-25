85a396c007d07962415fabf1678a923448c39b2a
krzysztof.ap.wilczynski@gmail.com <krzysztof.ap.wilczynski@gmail.com>
2025-02-25 23:48:09 +0100

    neotree, lualine, moduły

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,20 @@
+return {
+   "nvim-treesitter/nvim-treesitter", 
+  build = ":TSUpdate",
+
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      ensure_installed = {
+        "lua", "javascript", "bash", 
+        "css", "csv", "dockerfile", 
+        "graphql", "html", "http", 
+        "json", "markdown", "python", 
+        "scss", "sql", "typescript", 
+        "vue", "yaml"
+      },
+      highlight = {enable = true},
+      indent = {enable = true},
+    })
+  end
+}
