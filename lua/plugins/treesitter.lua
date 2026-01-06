a5fd148a2a94d6ef47c991e1c08dbf938a9e2c54
Krzysztof Wilczyński <krzysztof.ap.wilczynski@gmail.com>
2026-01-06 16:31:19 +0100

    downgrade treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,24 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "master",  -- stabilna wersja (nie main!)
   dependencies = {
     "hiphish/rainbow-delimiters.nvim",
+    "nvim-treesitter/nvim-treesitter-textobjects",
   },
   build = ":TSUpdate",
   lazy = false,
   config = function()
-    -- Auto-instalacja parserów (wymaga tree-sitter-cli)
-    require("nvim-treesitter").setup({
+    require("nvim-treesitter.configs").setup({
       ensure_installed = { "lua", "rust", "toml", "markdown", "json", "vim", "vimdoc" },
       auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+        },
+      },
     })
   end,
 }
